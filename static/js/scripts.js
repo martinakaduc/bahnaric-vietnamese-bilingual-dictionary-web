40d5ca89eeec89000f002d91882c6776eb1dcb6e
Phúc Nguyễn <nngiaphuc0101@gmail.com>
2022-11-13 14:27:47 +0700

    Added API call from TTS service, added a button in index.html for testing; a few debug console log is present, please edit

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,12 +1,3 @@
-/*!
-* Start Bootstrap - Creative v7.0.6 (https://startbootstrap.com/theme/creative)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
-
 window.addEventListener('DOMContentLoaded', event => {
 
     // Navbar shrink function
@@ -49,11 +40,6 @@ window.addEventListener('DOMContentLoaded', event => {
                 navbarToggler.click();
             }
         });
-    });
-
-    // Activate SimpleLightbox plugin for portfolio items
-    new SimpleLightbox({
-        elements: '#portfolio a.portfolio-box'
     });
 
 });
